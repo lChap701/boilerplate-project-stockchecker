4f34c0b3a436cc244d4f5f6196133226a682a700
lChap701 <>
2021-10-05 19:06:32 +0000

    Implemented helmet and formatted code

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,10 @@
-'use strict';
+"use strict";
 
-module.exports = function (app) {
+module.exports = function(app) {
 
-  app.route('/api/stock-prices')
-    .get(function (req, res){
-      
+  app.route("/api/stock-prices")
+    .get(function(req, res) {
+
     });
-    
+
 };
--- server.js
+++ server.js
@@ -1,26 +1,42 @@
-'use strict';
-require('dotenv').config();
-const express     = require('express');
-const bodyParser  = require('body-parser');
-const cors        = require('cors');
+"use strict";
+require("dotenv").config();
+const express= require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
-const apiRoutes         = require('./routes/api.js');
-const fccTestingRoutes  = require('./routes/fcctesting.js');
-const runner            = require('./test-runner');
+const apiRoutes = require("./routes/api.js");
+const fccTestingRoutes = require("./routes/fcctesting.js");
+const runner = require("./test-runner");
 
+/**
+ * Module that contains the entire application
+ * @module ./server
+ *
+ */
 const app = express();
 
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.use(cors({origin: '*'})); // For FCC testing purposes only
+app.use(cors({origin: "*"})); // For FCC testing purposes only
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Helmet Setup
+const helmet = require("helmet");
+app.use(helmet.xssFilter());
+app.use(helmet.noSniff());
+app.use(helmet.contentSecurityPolicy({ 
+  directives: {
+     defaultSrc: ["'self'"], 
+     scriptSrc: ["'self'"] 
+  }
+}));
+
 // Index page (static HTML)
-app.route('/')
+app.route("/")
   .get(function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
+    res.sendFile(process.cwd() + "/views/index.html");
   });
 
 // For FCC testing purposes
@@ -32,20 +48,20 @@ apiRoutes(app);
 // 404 Not Found Middleware
 app.use(function(req, res, next) {
   res.status(404)
-    .type('text')
-    .send('Not Found');
+    .type("text")
+    .send("Not Found");
 });
 
 // Start our server and tests!
 const listener = app.listen(process.env.PORT || 3000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
-  if(process.env.NODE_ENV==='test') {
-    console.log('Running Tests...');
+  console.log("Your app is listening on port " + listener.address().port);
+  if(process.env.NODE_ENV==="test") {
+    console.log("Running Tests...");
     setTimeout(function () {
       try {
         runner.run();
       } catch(e) {
-        console.log('Tests are not valid:');
+        console.log("Tests are not valid:");
         console.error(e);
       }
     }, 3500);
