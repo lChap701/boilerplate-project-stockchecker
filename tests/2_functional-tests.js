cd62323a428970553820106696ea86c5df1306ca
lChap701 <chapmanlucas201@gmail.com>
2021-10-14 16:18:46 -0500

    Added first test

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -5,6 +5,20 @@ const server = require("../server");
 
 chai.use(chaiHttp);
 
-suite("Functional Tests", function() {
+suite("Functional Tests", function () {
   /* My Tests */
+  const PATH = "/api/stock-prices";
+
+  suite("One Stock Tests", () => {
+    test("1)  Get Stock (Without Likes)", (done) => {
+      chai
+        .request(server)
+        .get(PATH + "?stock=GOOG")
+        .end((err, res) => {
+          assert.equal(res.status, 200, "response status should be 200");
+          assert.deepPropertyVal(JSON.parse(res.text), "stock", "GOOG", "response should return an object with a property of 'stock' that equals 'GOOG'");
+          done();
+        });
+    });
+  });
 });
