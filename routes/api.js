70a55cece5973bc06506456127d48d51f50f5ca0
lChap701 <chapmanlucas201@gmail.com>
2021-10-14 15:21:35 -0500

    Finished routing for the API

--- routes/api.js
+++ routes/api.js
@@ -1,4 +1,6 @@
 "use strict";
+const https = require("https");
+const cru = require("../cru");
 
 /**
  * Module that handles most of the routing
@@ -7,8 +9,47 @@
  * @param {*} app   Represents the Express application
  *
  */
-module.exports = function(app) {
+module.exports = function (app) {
   app.route("/api/stock-prices").get((req, res) => {
-    
+    const { stock } = req.query;
+    const API =
+      "https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/" +
+      stock +
+      "/quote";
+
+    // Calls freeCodeCamp's API and gets the price of the stock
+    https
+      .get(API, (resp) => {
+        let data = "";
+
+        resp.on("data", (chunk) => (data += chunk));
+
+        resp.on("end", () => {
+          const latestPrice = JSON.parse(data).latestPrice;
+
+          // Checks if stock needs to be created or updated and displays the stock
+          cru
+            .findStock(stock)
+            .then((stockObj) => {
+              if (req.query.like) {
+                cru
+                  .updateStock(stockObj._id, stockObj.likes + 1)
+                  .then(() => res.json({ stockData: stockObj }));
+              } else {
+                res.json({ stockData: stockObj });
+              }
+            })
+            .catch(() =>
+              cru
+                .addStock({
+                  stock: stock,
+                  price: latestPrice,
+                  likes: req.query.like ? 1 : 0,
+                })
+                .then((stockObj) => res.json({ stockData: stockObj }))
+            );
+        });
+      })
+      .on("error", (err) => res.send(err));
   });
 };
