72115f9517d2b1f0d12b41f757fdd142d6dda4ea
lChap701 <chapmanlucas201@gmail.com>
2021-10-14 20:30:15 -0500

    Added second test

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -10,7 +10,7 @@ suite("Functional Tests", function () {
   const PATH = "/api/stock-prices";
 
   suite("One Stock Tests", () => {
-    test("1)  Get Stock (Without Likes)", (done) => {
+    test("1)  Get Stock (without Likes)", (done) => {
       chai
         .request(server)
         .get(PATH + "?stock=GOOG")
@@ -21,6 +21,21 @@ suite("Functional Tests", function () {
             "stock",
             "GOOG",
             "response should return an object with a property of 'stock' that equals 'GOOG'"
+          );
+          done();
+        });
+    });
+
+    test("2)  Get Stock with Likes", (done) => {
+      chai
+        .request(server)
+        .get(PATH + "?stock=GOOG&like=true")
+        .end((err, res) => {
+          assert.equal(res.status, 200, "response status should be 200");
+          assert.notDeepEqual(
+            JSON.parse(res.text).stockData.likes,
+            0,
+            "response should return an object with a property of 'likes' that is greater than '0'"
           );
           done();
         });
