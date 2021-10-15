c3c0ff2c05eca876e0f0465b4ea876d652363e21
lChap701 <chapmanlucas201@gmail.com>
2021-10-15 11:21:18 -0500

    Added handlers for handling stocks

---
+++ handlers/oneStock.js
@@ -0,0 +1,91 @@
+const https = require("https");
+const cru = require("../cru");
+
+/**
+ * Module that gets and displays one stock
+ * @module ./handlers/oneStock
+ *
+ * @param {String} stock  Represents the stock to search for and display
+ * @param {String} like   Determines if the user likes a stock
+ * @param {String} ip     Represents the user's IP address
+ * @param {*} res         Represents the response
+ * 
+ */
+module.exports = (stock, like, ip, res) => {
+  // Calls freeCodeCamp's API to get the price of the stock
+  https
+    .get(
+      `https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/${stock}/quote`,
+      (resp) => {
+        let data = "";
+
+        resp.on("data", (chunk) => (data += chunk));
+
+        resp.on("end", () => {
+          const latestPrice = JSON.parse(data).latestPrice;
+
+          // Checks if stock needs to be created or updated and displays the stock
+          cru.findStock(stock.toUpperCase()).then((stockObj) => {
+            if (!stockObj) {
+              let likes = 0;
+              let ips = [];
+
+              if (like == "true") {
+                ips.push(ip);
+                likes++;
+              }
+
+              cru
+                .addStock({
+                  name: stock,
+                  price: latestPrice,
+                  likes: likes,
+                  ips: ips,
+                })
+                .then((stockObj) =>
+                  res.json({
+                    stockData: {
+                      stock: stockObj.name,
+                      price: stockObj.price,
+                      likes: stockObj.likes,
+                    },
+                  })
+                )
+                .catch((ex) => res.send(ex));
+              return;
+            }
+
+            if (like == "true" || stockObj.price != latestPrice) {
+              let likes = stockObj.likes;
+
+              if (like == "true" && !stockObj.ips.includes(ip)) {
+                stockObj.ips.push(ip);
+                likes++;
+              }
+
+              cru
+                .updateStock(stockObj._id, likes, latestPrice, stockObj.ips)
+                .then((newStock) =>
+                  res.json({
+                    stockData: {
+                      stock: newStock.name,
+                      price: newStock.price,
+                      likes: newStock.likes,
+                    },
+                  })
+                );
+            } else {
+              res.json({
+                stockData: {
+                  stock: stockObj.name,
+                  price: stockObj.price,
+                  likes: stockObj.likes,
+                },
+              });
+            }
+          });
+        });
+      }
+    )
+    .on("error", (err) => res.send(err));
+};
---
+++ handlers/twoStocks.js
@@ -0,0 +1,102 @@
+const https = require("https");
+const cru = require("../cru");
+
+/**
+ * Module that gets, compares, and displays two stocks
+ * @module ./handlers/twoStocks
+ *
+ * @param {[String]} stocks   Represents the stocks to search for and display
+ * @param {String} like       Determines if the user likes a stock
+ * @param {String} ip         Represents the user's IP address
+ * @param {*} res             Represents the response
+ *
+ */
+module.exports = (stocks, like, ip, res) => {
+  let stockData = [];
+
+  // Calls freeCodeCamp's API to get the price of each stock
+  stocks.forEach((stock) => {
+    https
+      .get(
+        `https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/${stock}/quote`,
+        (resp) => {
+          let data = "";
+
+          resp.on("data", (chunk) => (data += chunk));
+
+          resp.on("end", () => {
+            const latestPrice = JSON.parse(data).latestPrice;
+
+            // Checks if stock needs to be created or updated and displays the stock
+            cru.findStock(stock.toUpperCase()).then((stockObj) => {
+              if (!stockObj) {
+                let likes = 0;
+                let ips = [];
+
+                if (like == "true") {
+                  ips.push(ip);
+                  likes++;
+                }
+
+                cru
+                  .addStock({
+                    name: stock,
+                    price: latestPrice,
+                    likes: likes,
+                    ips: ips,
+                  })
+                  .then((stockObj) =>
+                    stockData.push({
+                      stock: stockObj.name,
+                      price: stockObj.price,
+                      rel_likes: stockObj.likes,
+                    })
+                  )
+                  .catch((ex) => {
+                    res.send(ex);
+                    return;
+                  });
+                return;
+              }
+
+              if (like == "true" || stockObj.price != latestPrice) {
+                let likes = stockObj.likes;
+
+                if (like == "true" && !stockObj.ips.includes(ip)) {
+                  stockObj.ips.push(ip);
+                  likes++;
+                }
+
+                cru
+                  .updateStock(stockObj._id, likes, latestPrice, stockObj.ips)
+                  .then((newStock) =>
+                    stockData.push({
+                      stock: newStock.name,
+                      price: newStock.price,
+                      rel_likes: newStock.likes,
+                    })
+                  );
+              } else {
+                stockData.push({
+                  stock: stockObj.name,
+                  price: stockObj.price,
+                  rel_likes: stockObj.likes,
+                });
+              }
+            });
+          });
+        }
+      )
+      .on("error", (err) => {
+        res.send(err);
+        return;
+      });
+  });
+
+  // Compares the two stocks relative likes
+  stockData.sort((a, b) => b.rel_likes - a.rel_likes);
+  stockData[0].rel_likes = stockData[0].rel_likes - stockData[1].rel_likes;
+  stockData[1].rel_likes = stockData[1].rel_likes - stockData[0].rel_likes;
+
+  res.json({ stockData: stockData });
+};
